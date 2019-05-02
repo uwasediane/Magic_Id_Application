61fec3ce7e506ae6feb7f33b17204a6405392978
Rwasibo <44880813+Rwasibo@users.noreply.github.com>
2019-05-02 20:36:08 +0200

    Magic Appication_Diane

---
+++ app/src/main/java/www/adc/rw/magicidapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package www.adc.rw.magicidapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
